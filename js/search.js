45f8566caada59558905b91c2ba4275db74bc20e
A-Abd <abrar.abdulwahed@gmail.com>
2022-01-26 16:40:37 +0300

    big changes

--- js/search.js
+++ js/search.js
@@ -3,14 +3,17 @@
 function search_book() {
     let input = document.getElementById('header_search').value;
     input=input.toLowerCase();
-    let x = document.getElementsByClassName('offer-text');
+    console.log(input);
+    let x = document.getElementsByClassName('card-text');
     for (i = 0; i < x.length; i++) { 
-        let parent = x[i].parentNode.parentElement.parentElement;
+        let parent = x[i].parentNode.parentElement;
         if (!x[i].innerText.toLowerCase().includes(input)) {
             parent.style.display="none";
+            console.log('inside if', parent);
         }
         else {
-            parent.style.display="list-item";                 
+            parent.style.display="list-item"; 
+            console.log('inside else', parent);                
         }
     }
 }
