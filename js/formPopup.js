667b30ae1492e838832b90c1fa785971de3013a1
A-Abd <abrar.abdulwahed@gmail.com>
2022-01-27 13:40:08 +0300

    prepare3 index.html

--- js/formPopup.js
+++ js/formPopup.js
@@ -1,184 +1,52 @@
-//DOM elements
-const DOMstrings = {
-    stepsBtnClass: 'multisteps-form__progress-btn',
-    stepsBtns: document.querySelectorAll(`.multisteps-form__progress-btn`),
-    stepsBar: document.querySelector('.multisteps-form__progress'),
-    stepsForm: document.querySelector('.multisteps-form__form'),
-    stepsFormTextareas: document.querySelectorAll('.multisteps-form__textarea'),
-    stepFormPanelClass: 'multisteps-form__panel',
-    stepFormPanels: document.querySelectorAll('.multisteps-form__panel'),
-    stepPrevBtnClass: 'js-btn-prev',
-    stepNextBtnClass: 'js-btn-next'
-  };
+const formBtn1 = document.querySelector("#btn-1")
+const formBtnPrev2 = document.querySelector("#btn-2-prev")
+const formBtnNext2 = document.querySelector("#btn-2-next")
+const formBtn3 = document.querySelector("#btn-3")
+// Button listener of form 1
+formBtn1.addEventListener("click", function(e) {
+    gotoNextForm(formBtn1, formBtnNext2, 1, 2)
+    e.preventDefault()
+  })
   
-  //remove class from a set of items
-  const removeClasses = (elemSet, className) => {
-    
-    elemSet.forEach(elem => {
-      
-      elem.classList.remove(className);
-      
-    });
-    
-  };
+  // Next button listener of form 2
+  formBtnNext2.addEventListener("click", function(e) {
+    gotoNextForm(formBtnNext2, formBtn3, 2, 3)
+    e.preventDefault()
+  })
   
-  //return exect parent node of the element
-  const findParent = (elem, parentClass) => {
-    
-    let currentNode = elem;
+  // Previous button listener of form 2
+  formBtnPrev2.addEventListener("click", function(e) {
+    gotoNextForm(formBtnNext2, formBtn1, 2, 1)
+    e.preventDefault()
+  })
   
-    while(! (currentNode.classList.contains(parentClass))) {
-      currentNode = currentNode.parentNode;
-    }
-    
-    return currentNode;
-    
-  };
-  
-  //get active button step number
-  const getActiveStep = elem => {
-    return Array.from(DOMstrings.stepsBtns).indexOf(elem);
-  };
-  
-  //set all steps before clicked (and clicked too) to active
-  const setActiveStep = (activeStepNum) => {
-    
-    //remove active state from all the state
-    removeClasses(DOMstrings.stepsBtns, 'js-active');
-    
-    //set picked items to active
-    DOMstrings.stepsBtns.forEach((elem, index) => {
-      
-      if(index <= (activeStepNum) ) {
-        elem.classList.add('js-active');
-      }
-      
-    });
-  };
-  
-  //get active panel
-  const getActivePanel = () => {
-    
-    let activePanel;
-    
-    DOMstrings.stepFormPanels.forEach(elem => {
-      
-      if(elem.classList.contains('js-active')) {
-        
-        activePanel = elem;
-        
-      }
-      
-    });
-    
-    return activePanel;
-                                      
-  };
-  
-  //open active panel (and close unactive panels)
-  const setActivePanel = activePanelNum => {
-    
-    //remove active class from all the panels
-    removeClasses(DOMstrings.stepFormPanels, 'js-active');
-    
-    //show active panel
-    DOMstrings.stepFormPanels.forEach((elem, index) => {
-      if(index === (activePanelNum)) {
-        
-        elem.classList.add('js-active');
-     
-        setFormHeight(elem);
-        
-      }
-    })
-    
-  };
-  
-  //set form height equal to current panel height
-  const formHeight = (activePanel) => {
-    
-    const activePanelHeight = activePanel.offsetHeight;
-    
-    DOMstrings.stepsForm.style.height = `${activePanelHeight}px`;
-    
-  };
-  
-  const setFormHeight = () => {
-    const activePanel = getActivePanel();
-  
-    formHeight(activePanel);
-  }
-  
-  //STEPS BAR CLICK FUNCTION
-  DOMstrings.stepsBar.addEventListener('click', e => {
-    
-    //check if click target is a step button
-    const eventTarget = e.target;
-    
-    if(!eventTarget.classList.contains(`${DOMstrings.stepsBtnClass}`)) {
-      return;
-    }
-    
-    //get active button step number
-    const activeStep = getActiveStep(eventTarget);
-    
-    //set all steps before clicked (and clicked too) to active
-    setActiveStep(activeStep);
-    
-    //open active panel
-    setActivePanel(activeStep);
-  });
-  
-  //PREV/NEXT BTNS CLICK
-  DOMstrings.stepsForm.addEventListener('click', e => {
-    
-    const eventTarget = e.target;
-    
-    //check if we clicked on `PREV` or NEXT` buttons
-    if(! ( (eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`)) || (eventTarget.classList.contains(`${DOMstrings.stepNextBtnClass}`)) ) ) 
-    {
-      return;
-    }
-    
-    //find active panel
-    const activePanel = findParent(eventTarget, `${DOMstrings.stepFormPanelClass}`);
-    
-    let activePanelNum = Array.from(DOMstrings.stepFormPanels).indexOf(activePanel);
-    
-    //set active step and active panel onclick
-    if(eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`)) {
-      activePanelNum--;
-    
-    } else {
-      
-      activePanelNum++;
-    
-    }
-    
-    setActiveStep(activePanelNum);
-    setActivePanel(activePanelNum);
-    
-  });
-  
-  //SETTING PROPER FORM HEIGHT ONLOAD
-  window.addEventListener('load', setFormHeight, false);
-  
-  //SETTING PROPER FORM HEIGHT ONRESIZE
-  window.addEventListener('resize', setFormHeight, false);
-  
-  //changing animation via animation select !!!YOU DON'T NEED THIS CODE (if you want to change animation type, just change form panels data-attr)
-  
-  const setAnimationType = (newType) => {
-    DOMstrings.stepFormPanels.forEach(elem => {
-      elem.dataset.animation = newType;
-    })
-  };
-  
-  //selector onchange - changing animation
-  const animationSelect = document.querySelector('.pick-animation__select');
-  
-  animationSelect.addEventListener('change', () => {
-    const newAnimationType = animationSelect.value;
-    
-    setAnimationType(newAnimationType);
-  });
+  // Button listener of form 3
+  formBtn3.addEventListener("click", function(e) {
+    document.querySelector(`.step--3`).classList.remove("step-active")
+    document.querySelector(`.step--4`).classList.add("step-active")
+    formBtn3.parentElement.style.display = "none"
+    document.querySelector(".form--message").innerHTML = `
+     <h1 class="form--message-text">Your account is successfully created </h1>
+     `
+    e.preventDefault()
+  })
+  const gotoNextForm = (prev, next, stepPrev, stepNext) => {
+  // Get form through the button
+  const prevForm = prev.parentElement
+  const nextForm = next.parentElement
+  const nextStep = document.querySelector(`.step--${stepNext}`)
+  const prevStep = document.querySelector(`.step--${stepPrev}`)
+  // Add active/inactive classes to both previous and next form
+  nextForm.classList.add("form-active")
+  nextForm.classList.add("form-active-animate")
+  prevForm.classList.add("form-inactive")
+  // Change the active step element
+  prevStep.classList.remove("step-active")
+  nextStep.classList.add("step-active")
+  // Remove active/inactive classes to both previous an next form
+  setTimeout(() => {
+    prevForm.classList.remove("form-active")
+    prevForm.classList.remove("form-inactive")
+    nextForm.classList.remove("form-active-animate")
+  }, 1000)
+}
