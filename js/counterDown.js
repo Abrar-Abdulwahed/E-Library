593cb203c7cfa89d276ed75f944573e41198c926
A-Abd <abrar.abdulwahed@gmail.com>
2022-01-27 05:53:02 +0300

    prepare index.html

---
+++ js/counterDown.js
@@ -0,0 +1,38 @@
+function randomDate(start, end) {
+    return new Date(+start + Math.random() * (end - start));
+}
+
+var randomDateVar, distance;
+const second = 1000;
+const minutes = second * 60;
+const hours = minutes * 60;
+const days = hours * 24;
+const now = new Date().getTime();
+
+const getDistance =(randDate)=>{
+    return randDate - now;
+}
+const getRemainDays = (distance)=>{
+    return Math.floor(distance / days);
+}
+const getRemainHours = (distance)=>{
+    return Math.floor((distance % days) / hours);
+}
+const getRemainMinutes = (distance)=>{
+    return Math.floor((distance % hours) / minutes);
+}
+const getRemainSeconds = (distance)=>{
+    return Math.floor((distance % minutes) / second);
+}
+const countDownFunc = (distance)=> {
+    var noOfChildren = document.querySelectorAll('.offer-section .card-item');
+    for(let i=0; i < noOfChildren.length; i++){
+        randomDateVar = randomDate(new Date().getTime(), new Date(2022, 11, 20));
+        distance = getDistance(randomDateVar);
+        document.getElementById(`day${i+1}`).innerHTML = getRemainDays(distance);
+        document.getElementById(`hours${i+1}`).innerHTML = getRemainHours(distance);
+        document.getElementById(`min${i+1}`).innerHTML = getRemainMinutes(distance);
+        document.getElementById(`sec${i+1}`).innerHTML = getRemainSeconds(distance);
+    }
+}
+countDownFunc();
